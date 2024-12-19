d6c12fd6c85b50d41f07d6782b715b248cc8f7e9
Vladislav-Zakrevskiy <zakrevskiyvlada@yandex.ru>
2024-12-19 17:04:21 +0300

    ркутоуоуо

--- project/project/settings.py
+++ project/project/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),  # For dubug
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
